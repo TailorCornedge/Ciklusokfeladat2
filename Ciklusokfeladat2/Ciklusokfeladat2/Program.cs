6458b3ceaf609ffc750d2147a3b74c1e033de402
Tailor Cornedge <szabok2@kkszki.hu>
2025-09-25 08:02:09 +0200

    feladat is ready

--- Ciklusokfeladat2/Ciklusokfeladat2/Program.cs
+++ Ciklusokfeladat2/Ciklusokfeladat2/Program.cs
@@ -10,6 +10,18 @@ namespace Ciklusokfeladat2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adja megy az 1. számot: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a 2. számot: ");
+            int y = int.Parse(Console.ReadLine());
+            if (x>y)
+            {
+                Console.WriteLine($"{x} a nagyobb szám. A két szám közt lévő mennyiség: {x - y - 1}");
+            }
+            else
+            {
+                Console.WriteLine($"{y} a nagyobb szám. A két szám közt lévő mennyiség: {y - x - 1}");
+            }
         }
     }
 }
